41df04a1dfe6ff570c5813c9317cefdf55cd4c0d
kevin0493559 <hg846@nyu.edu>
2014-03-25 11:56:16 -0400

    Initial Commit

---
+++ PickYourClothes/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  PickYourClothes
+//
+//  Created by Rui Yao on 3/25/14.
+//  Copyright (c) 2014 HAO GU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PickYourClothesTests/PickYourClothesTests.m
@@ -0,0 +1,34 @@
+//
+//  PickYourClothesTests.m
+//  PickYourClothesTests
+//
+//  Created by Rui Yao on 3/25/14.
+//  Copyright (c) 2014 HAO GU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PickYourClothesTests : XCTestCase
+
+@end
+
+@implementation PickYourClothesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
